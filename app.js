e309e5fa52a1723837497bed4fda34d3e7b22542
Dan Crews <crewsd@gmail.com>
2013-09-28 20:09:53 -0600

    "use strict" is good. Also adding hint globals for linting.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*global Buffer:false,clearInterval:false,clearTimeout:false,console:false,exports:false,global:false,module:false,process:false,querystring:false,require:false,setInterval:false,setTimeout:false,__filename:false,__dirname:false */
+'use strict';
+
 /**
  * Deps
  */
