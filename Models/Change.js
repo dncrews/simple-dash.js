e6d2c4942d7fdbe8d3d747f6b98b95e341376b3f
Dan Crews <crewsd@gmail.com>
2013-12-23 09:07:33 -0700

    Limiting history dates. Fixing repo_name in Changes

--- Models/Change.js
+++ Models/Change.js
@@ -134,7 +134,7 @@ ChangeSchema.statics.fromJenkins = function(data, action) {
       name : data.name
     };
 
-  config.repo_name = data.name.replace('fs-', '');
+  config.repo_name = data.name.replace('fs-', '').replace('-prod', '');
 
   debug('FromJenkins: ', config);
 
