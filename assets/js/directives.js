88eec4ee79ad2ad0bdbf87b6362d75730c81215a
Dan Crews <crewsd@gmail.com>
2014-01-06 11:53:31 -0700

    Whitespace change (lint)

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -55,13 +55,13 @@
         //Shade the color with a magnitude of SHADE_MAGNITUDE by shifting bits
         //Based upon: http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color
         function shadeColor(color, percent) {
-            var num = parseInt(color.slice(1),16)
-              , amt = Math.round(SHADE_MAGNITUDE * percent)
-              , R = (num >> 16) + amt
-              , B = (num >> 8 & 0x00FF) + amt
-              , G = (num & 0x0000FF) + amt;
-            var newColor = "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
-            return newColor;
+          var num = parseInt(color.slice(1),16)
+            , amt = Math.round(SHADE_MAGNITUDE * percent)
+            , R = (num >> 16) + amt
+            , B = (num >> 8 & 0x00FF) + amt
+            , G = (num & 0x0000FF) + amt;
+          var newColor = "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+          return newColor;
         }
 
         scope.mouseIn = function() {
